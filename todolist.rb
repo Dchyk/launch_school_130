057952a40e6b6068d275d1993ab136c60f28ffa2
Alex Dzwonchyk <alex.dzwonchyk@gmail.com>
2017-08-08 09:01:12 -0400

    Todolist 1

--- todolist.rb
+++ todolist.rb
@@ -28,12 +28,84 @@ class Todo
 end
 
 class TodoList
-  attr_accessor :title
+  attr_accessor :title, :todos
 
-  def intitialize(title)
+  def initialize(title)
     @title = title
     @todos = []
   end
 
-# rest of class needs implementation
+  # rest of class needs implementation
+
+  def add(item)
+    if item.instance_of?(Todo)
+      self.todos << item
+    else
+      raise TypeError, "Can only add Todo objects"
+    end
+  end
+  alias_method :<<, :add
+
+  def size
+    todos.size
+  end
+
+  def first
+    todos.first
+  end
+
+  def last
+    todos.last
+  end
+
+  def item_at(index)
+    todos.fetch(index)
+  end
+
+  def mark_done_at(index)
+    todos.fetch(index).done!
+  end
+
+  def mark_undone_at(index)
+    todos.fetch(index).undone!
+  end
+
+  # Deleting from the list
+
+  def shift
+    todos.shift
+  end
+
+  def pop
+    todos.pop
+  end
+
+  def remove_at(index)
+    todos.delete_at(index) unless todos.fetch(index) == IndexError
+  end
+
+  # Outputting the list
+
+  def to_s
+    puts "---- #{title} ----\n"
+    todos.each { |item| puts item }
+  end
+
+  def to_a
+    todos
+  end
+
+  # Each
+
+  def each
+    counter = 0
+
+    while counter < todos.size
+      yield todos[counter]
+      counter += 1
+    end
+
+    todos
+  end
+
 end
